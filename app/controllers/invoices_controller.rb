1f78e4737683b0ee01f8c7cb3071685e7cd4f88c
Pavan Mallela <mallelapavank@gmail.com>
2022-07-18 19:32:13 +0530

    create new invoice form; create controller action

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -6,4 +6,15 @@ class InvoicesController < ApplicationController
   def new
     @invoice = Invoice.new
   end
+
+  def create
+    @invoice = current_user.invoices.new(invoice_params)
+    # TODO: Add items as nested attributes
+    @invoice.save!
+  end
+
+  private
+  def invoice_params
+    params.require(:invoice).permit(:invoice_number)
+  end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,8 @@
 class Invoice < ApplicationRecord
   has_many :items
-	belongs_to :user
+  belongs_to :user
+
+  # accepts_nested_attributes_for :items
+  
+  validates :invoice_number, presence: true, length: { minimum: 3 }, uniqueness: { case_sensitive: false }
 end
